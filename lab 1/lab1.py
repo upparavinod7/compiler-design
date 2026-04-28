b4813f04661e1c784095acfe7b4d323fbac976d8
upparavinod7 <upparavinod7@gmai.com>
2026-04-28 10:28:59 +0530

    lab -1 finished

--- lab 1/lab1.py
+++ lab 1/lab1.py
@@ -5,4 +5,31 @@ def add_symbol(name, value):
     else:
         symbol_table[name]=value
         print("symbol added")
-    
+def update_symbol(name,value):
+    if name in symbol_table:
+        symbol_table[name]=value
+        print("symbol updated")
+    else:
+        print("symbol not found")
+def find_symbol(name):
+    if name in symbol_table:
+        print("symbol found value", symbol_table[name])
+    else:
+        print("Symbol not found")
+def delete_symbol(name):
+    if name in symbol_table:
+        del symbol_table[name]
+        print("symbol deleted")
+    else:
+        print("symbol not found")
+def show_symbols():
+    for name in symbol_table:
+        print("name", name, "value", symbol_table[name])
+add_symbol("x", 20)
+add_symbol("y" , 30)
+add_symbol("z" , 40)
+show_symbols()
+update_symbol("x", 10)
+show_symbols()
+find_symbol("y")
+delete_symbol("z")
