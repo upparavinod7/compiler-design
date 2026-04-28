3fe51d7ce05e0f0f66a24b20cc963f378c5a2d0f
upparavinod7 <upparavinod7@gmail.com>
2026-04-28 22:21:29 +0530

    lab 2 complete

--- lab 2/lab2.py
+++ lab 2/lab2.py
@@ -1,2 +1,60 @@
-print("Hii")
-print("hello")
+import keyword
+
+def check_keyword(word):
+    if word in keyword.kwlist:
+        print(word, "is a keyword")
+    else:
+        print(word, "is identifier")
+
+code = """
+x=10
+y=x+20
+print(y)
+"""
+
+numbers = []
+identifiers = []
+special_chars = []
+lineno = 1
+i = 0
+
+while i < len(code):
+    c = code[i]
+
+    if c.isdigit():
+        token_value = c
+        i += 1
+        while i < len(code) and code[i].isdigit():
+            token_value += code[i]
+            i += 1
+        numbers.append(int(token_value))
+        continue
+
+    if c.isalpha() or c == '_':
+        word = c
+        i += 1
+        while i < len(code) and (code[i].isalnum() or code[i] == '_'):
+            word += code[i]
+            i += 1
+        identifiers.append(word)
+        continue
+
+    if c in [' ', '\t']:
+        i += 1
+        continue
+
+    if c == '\n':
+        lineno += 1
+        i += 1
+        continue
+
+    special_chars.append(c)
+    i += 1
+print("Numbers in the program:", numbers)
+
+print("\nKeywords and identifiers:")
+for word in identifiers:
+    check_keyword(word)
+
+print("\nSpecial characters:", special_chars)
+print("\nTotal number of lines:", lineno)
